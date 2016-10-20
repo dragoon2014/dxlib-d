f28116523c244e002498834335e684e53bbf1324
dragoon2014 <dragoon2014@users.noreply.github.com>
2016-10-20 17:43:36 +0900

    dxlib-d.sh support -u option: skip cp932 to utf8 conversion

--- dxlib-d.sh
+++ dxlib-d.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 hdrs=(DxLib DxFunctionWin)
 
+isUtf8=0
+
+for arg in $*; do
+    case $arg in
+        -u)
+            # skip cp932 to utf8 conversion
+            isUtf8=1
+            ;;
+        *)
+            ;;
+    esac
+done
+
 for ((i=0; i<${#hdrs[@]}; i++)){
     src="${hdrs[i]}.h"
     dst="${hdrs[i],,}.d"
@@ -11,7 +24,7 @@ for ((i=0; i<${#hdrs[@]}; i++)){
         cat template_winapi.d >> $dst
     fi
 
-    iconv -f cp932 -t utf-8 $src \
+    ([[ $isUtf8 -eq 1 ]] && cat $src || iconv -f cp932 -t utf-8 $src) \
         | sed -f replace_simply.sed \
         | bash convert_typedef.sh >> $dst
 }
