7819c72a1e0cf5b09f3883b67888ee5e8d1d480d
dragoon2014 <dragoon2014@users.noreply.github.com>
2025-03-23 00:17:05 +0900

    Modify convert logic, no output change

--- dxlib-d.sh
+++ dxlib-d.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-hdrs=(DxLib DxFunctionWin)
 
 isUtf8=0
 
@@ -14,23 +13,23 @@ for arg in "$@"; do
     esac
 done
 
+if [[ $isUtf8 -eq 1 ]]; then
+    utf8ed_cat='cat'
+else
+    utf8ed_cat='iconv -f cp932 -t utf-8'
+fi
+
 dst="dxlib.d"
+
+convert_to_D(){
+    echo converting "$1"... >&2
+    $utf8ed_cat "$1" \
+        | tr -d '\r' \
+        | sed -f replace_simply.sed \
+        | bash convert_typedef.sh
+}
 
 echo "module dxlib;" > "$dst"
 cat template_winapi.d >> "$dst"
-
-for ((i=0; i<${#hdrs[@]}; i++)){
-    src="${hdrs[i]}.h"
-    echo converting "$src"...
-
-    if [[ $isUtf8 -eq 1 ]]; then
-        utf8ed_cat='cat'
-    else
-        utf8ed_cat='iconv -f cp932 -t utf-8'
-    fi
-
-    $utf8ed_cat "$src" \
-        | tr -d '\r' \
-        | sed -f replace_simply.sed \
-        | bash convert_typedef.sh >> "$dst"
-}
+convert_to_D DxLib.h >> $dst
+convert_to_D DxFunctionWin.h >> $dst
